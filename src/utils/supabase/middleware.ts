5b751f39bc9041be7f428eb6a6baa05f9a8f598c
Eun Oh <bdohhhhh@gmail.com>
2024-09-20 16:53:36 +0900

    [add]: 수파베이스 설정 파일들 추가

---
+++ src/utils/supabase/middleware.ts
@@ -0,0 +1,60 @@
+import { createServerClient } from "@supabase/ssr";
+import { NextResponse, type NextRequest } from "next/server";
+
+export async function updateSession(request: NextRequest) {
+    const requestHeaders = new Headers(request.headers);
+
+    let supabaseResponse = NextResponse.next({
+        request,
+    });
+
+    const supabase = createServerClient(
+        process.env.NEXT_PUBLIC_SUPABASE_URL!,
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+        {
+            cookies: {
+                getAll() {
+                    return request.cookies.getAll();
+                },
+                setAll(cookiesToSet) {
+                    cookiesToSet.forEach(({ name, value, options }) => request.cookies.set(name, value));
+                    supabaseResponse = NextResponse.next({
+                        request,
+                    });
+                    cookiesToSet.forEach(({ name, value, options }) =>
+                        supabaseResponse.cookies.set(name, value, options)
+                    );
+                },
+            },
+        }
+    );
+
+    const {
+        data: { user },
+    } = await supabase.auth.getUser();
+
+    if (
+        !user &&
+        request.nextUrl.pathname !== "/" &&
+        !request.nextUrl.pathname.startsWith("/vankoadmin") &&
+        !request.nextUrl.pathname.startsWith("/api")
+    ) {
+        // no user, potentially respond by redirecting the user to the login page
+        const url = request.nextUrl.clone();
+        url.pathname = "/admin";
+        return NextResponse.redirect(url);
+    }
+
+    if (user) {
+        requestHeaders.set("x-user", user.id);
+
+        // 새로운 응답 객체 생성
+        supabaseResponse = NextResponse.next({
+            request: {
+                headers: requestHeaders,
+            },
+        });
+    }
+
+    return supabaseResponse;
+}
