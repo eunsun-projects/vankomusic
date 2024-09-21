ee5fc52b2ab9dd9c94ea4a7e5527beb87f6c3340
Eun Oh <bdohhhhh@gmail.com>
2024-09-21 15:10:34 +0900

    [add]: genTypes supabase

---
+++ src/types/vanko.type.ts
@@ -0,0 +1,7 @@
+import { Tables } from './supabase';
+
+export type Audios = Tables<'audios'>;
+export type Users = Tables<'users'>;
+export type Videos = Tables<'videos'>;
+export type Wishs = Tables<'wishs'>;
+export type Visits = Tables<'visits'>;
