8602112c893392b541c07a70ad5f7a48fbae9c1b
Eun Oh <bdohhhhh@gmail.com>
2024-09-21 14:48:06 +0900

    [update]: navbar > typescript

---
+++ app/navbar.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import styles from "./page.module.css";
+
+export default function Navbar() {
+  return (
+    <div
+      className={styles.temp}
+      style={{
+        fontFamily: "DungGeunMo",
+        fontSize: "2rem",
+        display: "flex",
+        flexDirection: "column",
+        gap: "2rem",
+        padding: "1rem",
+      }}
+    >
+      <Link href="/" style={{ width: "fit-content", border: "2px dotted" }}>
+        temp
+      </Link>
+      <Link href="/home">home</Link>
+      <Link href="/seonang">서낭축원</Link>
+      <Link href="/ffd">FFD</Link>
+      <Link href="/modernmove">modernmove</Link>
+      <Link href="/circles">circles</Link>
+    </div>
+  );
+}
