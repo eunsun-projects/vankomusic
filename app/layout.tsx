ac87284b37daa71bc596cde890a8bbc34f07e303
Eun Oh <bdohhhhh@gmail.com>
2024-09-21 15:44:21 +0900

    [add]: query provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,15 @@
-import AuthContext from "@/context/AuthContext";
-import { orbitron } from "@/fonts";
-import { Analytics } from "@vercel/analytics/react";
-import "./globals.css";
+import { orbitron } from '@/fonts';
+import { Analytics } from '@vercel/analytics/react';
+import './globals.css';
 
-import GoogleAnalytics from "./googleAnalytics";
+import QueryProvider from '@/providers/query.provider';
+import GoogleAnalytics from './googleAnalytics';
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="kr" className={`${orbitron.className}`}>
       <body>
-        <AuthContext>{children}</AuthContext>
+        <QueryProvider>{children}</QueryProvider>
         <Analytics />
         <GoogleAnalytics />
       </body>
