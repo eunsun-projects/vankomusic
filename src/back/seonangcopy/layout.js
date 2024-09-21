a14b58fb80bad2aa01cd9481c693a330c4c1e81b
Eun Oh <bdohhhhh@gmail.com>
2024-09-21 15:42:57 +0900

    [del]

--- src/back/seonangcopy/layout.js
+++
@@ -1,9 +0,0 @@
-import '../globals.css'
-
-export default function SeonangLayout({ children }) {
-    return (
-        <div style={{height : '100%', minHeight: "100vh", overflowY:"auto", backgroundImage: 'url(/assets/img/cave2.jpg)', backgroundSize: 'cover', touchAction: "none"}}>
-            {children}
-        </div>
-    )
-}
