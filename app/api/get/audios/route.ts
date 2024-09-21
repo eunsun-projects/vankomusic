9a06d87b0e9de7740ccc214b94f4fdb6727b994a
Eun Oh <bdohhhhh@gmail.com>
2024-09-21 15:10:50 +0900

    [add]: get audios route

---
+++ app/api/get/audios/route.ts
@@ -0,0 +1,17 @@
+import { Audios } from '@/types/vanko.type';
+import { createClient } from '@/utils/supabase/client';
+import { PostgrestError } from '@supabase/supabase-js';
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+  const supabase = createClient();
+
+  const { data: audios, error }: { data: Audios[] | null; error: PostgrestError | null } =
+    await supabase.from('audios').select('*');
+
+  if (error) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+
+  return NextResponse.json(audios, { status: 200 });
+}
