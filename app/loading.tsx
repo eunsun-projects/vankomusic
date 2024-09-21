7ea0ffc1e44eb96e44c863240b1c0434357e43ea
Eun Oh <bdohhhhh@gmail.com>
2024-09-21 14:47:02 +0900

    [update]: loading > typescript

---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+import styles from "@/app/page.module.css";
+
+export default function Loading() {
+  return (
+    <div className={styles.loader}>
+      <img className={styles.loaderimg} src="/assets/img/192vanko.png"></img>
+    </div>
+  );
+}
