e54a8e164dddb0eea59600106558b24d001eae4c
Eun Oh <bdohhhhh@gmail.com>
2024-09-21 21:48:12 +0900

    [update]: footer > typescript

---
+++ src/components/common/footer.tsx
@@ -0,0 +1,30 @@
+import styles from '@/app/page.module.css';
+import { orbitron } from '@/fonts';
+import Image from 'next/image';
+import Link from 'next/link';
+
+interface FooterProps {
+  color: string;
+}
+
+export default function Footer({ color }: FooterProps) {
+  return (
+    <div
+      className={`${styles.footer} ${orbitron.className}`}
+      style={{ borderTop: `1px solid ${color}` }}
+    >
+      <Link href="/" style={{ textDecoration: 'none' }}>
+        <div className={styles.footerimg}>
+          <Image
+            style={{ width: '1.2rem', height: '1.2rem' }}
+            src="/assets/img/footer_vanko.webp"
+            alt="vanko"
+            unoptimized
+          ></Image>
+          <p>home</p>
+        </div>
+      </Link>
+      <p>ⓒ2024.vankomusic.</p>
+    </div>
+  );
+}
