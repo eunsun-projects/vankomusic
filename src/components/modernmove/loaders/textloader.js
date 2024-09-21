144e0a4b3b31fb86099537a617b38ed1fddec343
Eun Oh <bdohhhhh@gmail.com>
2024-09-22 07:41:34 +0900

    [del]

--- src/components/modernmove/loaders/textloader.js
+++
@@ -1,6 +0,0 @@
-import { useProgress, Html } from "@react-three/drei"
-
-export default function TextLoader() {
-    const { active, progress, errors, item, loaded, total } = useProgress()
-    return <Html center>{progress} % loaded</Html>
-}
