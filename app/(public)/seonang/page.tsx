670dca8dc9d608e1c9e5df67c118dbb7c5fff36d
Eun Oh <bdohhhhh@gmail.com>
2024-09-22 09:21:41 +0900

    [update]: seonang page, layout > typescript

---
+++ app/(public)/seonang/page.tsx
@@ -0,0 +1,9 @@
+import { SeonangTemplate } from '@/components/project/seonang';
+import { basicMeta, basicViewport } from '@/meta/basicmeta';
+
+export const metadata = basicMeta;
+export const viewport = basicViewport;
+
+export default function SeonangPage() {
+  return <SeonangTemplate />;
+}
