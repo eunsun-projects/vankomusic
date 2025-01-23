4d405aa773365e8344087a7211596171e9b3c2ca
Eun Oh <bdohhhhh@gmail.com>
2025-01-23 17:30:02 +0900

    [update]: zustand 로직 업데이트

--- src/stores/zustand.ts
+++ src/stores/zustand.ts
@@ -1,6 +1,4 @@
 import { FocusedObject, TimeCapsule } from '@/types/projects.type';
-import { generateColor } from '@/utils/projects/timecapsule/generateColor';
-import { generateRandomPosition } from '@/utils/projects/timecapsule/generatePosition';
 import * as THREE from 'three';
 import { create } from 'zustand';
 
@@ -8,31 +6,17 @@ export interface TimeCapsuleState {
   focusedObject: FocusedObject | null;
   timeCapsules: TimeCapsule[];
   setFocusedObject: (focusedObject: FocusedObject | null) => void;
-  setTimeCapsules: (timeCapsule: TimeCapsule) => void;
+  setTimeCapsules: (timeCapsules: TimeCapsule[]) => void;
   updateTimeCapsule: (object: THREE.Mesh) => void;
+  addTimeCapsule: (timeCapsule: TimeCapsule) => void;
 }
-
-const makeInitialTimecapSules = () => {
-  return Array.from({ length: 10 }, () => ({
-    userId: 'user123',
-    title: 'New Capsule',
-    description: 'This is a new time capsule',
-    password: 'secure',
-    createdAt: new Date().toISOString(),
-    updatedAt: new Date().toISOString(),
-    position: [generateRandomPosition(), generateRandomPosition(), generateRandomPosition()],
-    color: generateColor(),
-    object: null,
-  }));
-};
-
-const initialTimeCapsules = makeInitialTimecapSules();
 
 export const useTimeCapsuleStore = create<TimeCapsuleState>((set) => ({
   focusedObject: null,
-  timeCapsules: initialTimeCapsules,
+  timeCapsules: [],
   setFocusedObject: (focusedObject: FocusedObject | null) => set({ focusedObject }),
-  setTimeCapsules: (timeCapsule: TimeCapsule) =>
+  setTimeCapsules: (timeCapsules: TimeCapsule[]) => set({ timeCapsules }),
+  addTimeCapsule: (timeCapsule: TimeCapsule) =>
     set((state) => ({
       timeCapsules: [...(state.timeCapsules || []), timeCapsule],
     })),
@@ -43,3 +27,19 @@ export const useTimeCapsuleStore = create<TimeCapsuleState>((set) => ({
       ),
     })),
 }));
+
+// const makeInitialTimecapSules = () => {
+//   return Array.from({ length: 10 }, () => ({
+//     userId: 'user123',
+//     title: 'New Capsule',
+//     description: 'This is a new time capsule',
+//     password: 'secure',
+//     createdAt: new Date().toISOString(),
+//     updatedAt: new Date().toISOString(),
+//     position: [generateRandomPosition(), generateRandomPosition(), generateRandomPosition()],
+//     color: generateColor(),
+//     object: null,
+//   }));
+// };
+
+// const initialTimeCapsules = makeInitialTimecapSules();
