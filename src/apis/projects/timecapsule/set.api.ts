315a74384483f1ab9d2936c306ed75898f0f1860
Eun Oh <bdohhhhh@gmail.com>
2025-01-23 18:25:56 +0900

    [update]: u, d - api, 뮤테이션 작성

--- src/apis/projects/timecapsule/set.api.ts
+++ src/apis/projects/timecapsule/set.api.ts
@@ -10,3 +10,23 @@ export async function postTimeCapsule(timeCapsule: Partial<TimeCapsule>) {
   });
   return data;
 }
+
+export async function editTimeCapsule(timeCapsule: Partial<TimeCapsule>) {
+  const url = '/api/set/timecapsules';
+  const data = await fetchWrapper<TimeCapsule>(url, {
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(timeCapsule),
+  });
+  return data;
+}
+
+export async function deleteTimeCapsule(timeCapsule: Partial<TimeCapsule>) {
+  const url = '/api/set/timecapsules';
+  const data = await fetchWrapper<TimeCapsule>(url, {
+    method: 'DELETE',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(timeCapsule),
+  });
+  return data;
+}
--- src/hooks/mutations/timecapsules.mutation.ts
+++ src/hooks/mutations/timecapsules.mutation.ts
@@ -1,4 +1,8 @@
-import { postTimeCapsule } from '@/apis/projects/timecapsule/set.api';
+import {
+  deleteTimeCapsule,
+  editTimeCapsule,
+  postTimeCapsule,
+} from '@/apis/projects/timecapsule/set.api';
 import { QUERY_KEY_TIME_CAPSULES } from '@/constants/query.constant';
 import { TimeCapsule } from '@/types/projects.type';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
@@ -7,6 +11,26 @@ export function useTimeCapsulesMutation() {
   const queryClient = useQueryClient();
   return useMutation<TimeCapsule, Error, Partial<TimeCapsule>>({
     mutationFn: (timeCapsule) => postTimeCapsule(timeCapsule),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [QUERY_KEY_TIME_CAPSULES] });
+    },
+  });
+}
+
+export function useEditTimeCapsulesMutation() {
+  const queryClient = useQueryClient();
+  return useMutation<TimeCapsule, Error, Partial<TimeCapsule>>({
+    mutationFn: (timeCapsule) => editTimeCapsule(timeCapsule),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [QUERY_KEY_TIME_CAPSULES] });
+    },
+  });
+}
+
+export function useDeleteTimeCapsulesMutation() {
+  const queryClient = useQueryClient();
+  return useMutation<TimeCapsule, Error, Partial<TimeCapsule>>({
+    mutationFn: (timeCapsule) => deleteTimeCapsule(timeCapsule),
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: [QUERY_KEY_TIME_CAPSULES] });
     },
