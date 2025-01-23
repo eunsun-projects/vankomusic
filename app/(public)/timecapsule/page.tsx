2a8060250759945af993a327e18443c46394a520
Eun Oh <bdohhhhh@gmail.com>
2025-01-23 17:30:43 +0900

    [update]: prefetch 추가

--- app/(public)/timecapsule/page.tsx
+++ app/(public)/timecapsule/page.tsx
@@ -1,7 +1,8 @@
 import { postUserServer } from '@/apis/auth/post.api';
+import { getTimeCapsules } from '@/apis/projects/timecapsule/get.api';
 import Loading from '@/app/loading';
 import TimeCapsuleTemplate from '@/components/project/timecapsule/TimeCapsuleTemplate';
-import { QUERY_KEY_USER } from '@/constants/query.constant';
+import { QUERY_KEY_TIME_CAPSULES, QUERY_KEY_USER } from '@/constants/query.constant';
 import { basicMeta, basicViewport } from '@/meta/basicmeta';
 import { getUserFromHeaders } from '@/utils/common/getUserByHeaders';
 import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
@@ -17,6 +18,11 @@ async function TimeCapsulePage() {
   await queryClient.prefetchQuery({
     queryKey: [QUERY_KEY_USER],
     queryFn: () => postUserServer(userId),
+  });
+
+  await queryClient.prefetchQuery({
+    queryKey: [QUERY_KEY_TIME_CAPSULES],
+    queryFn: () => getTimeCapsules(),
   });
 
   const dehydratedState = dehydrate(queryClient);
