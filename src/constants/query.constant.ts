1534c80004510a38608cce40647adb3ad8b3df17
Eun Oh <bdohhhhh@gmail.com>
2025-01-23 17:29:03 +0900

    [chore]

--- src/constants/query.constant.ts
+++ src/constants/query.constant.ts
@@ -3,3 +3,4 @@ export const QUERY_KEY_VIDEOS = 'videos';
 export const QUERY_KEY_WISHES = 'wishs';
 export const QUERY_KEY_USER = 'user';
 export const QUERY_KEY_CURATIONS = 'curations';
+export const QUERY_KEY_TIME_CAPSULES = 'timecapsules';
