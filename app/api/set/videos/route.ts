4fc716c29ff77ec86baf3287cd24da837961d75b
Eun Oh <bdohhhhh@gmail.com>
2025-02-23 19:44:01 +0900

    [fix]: videos set route 타입오류 수정

--- app/api/set/videos/route.ts
+++ app/api/set/videos/route.ts
@@ -43,6 +43,10 @@ export async function DELETE(request: NextRequest) {
   const videos: PartialVideos = await request.json();
   const supabase = await createClient();
 
+  if (!videos.id) {
+    return NextResponse.json({ error: 'id is required' }, { status: 400 });
+  }
+
   const { data, error }: { data: Videos | null; error: PostgrestError | null } = await supabase
     .from('videos')
     .update({ isPublic: false })
