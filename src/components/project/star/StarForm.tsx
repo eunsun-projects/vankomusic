e22d4614f3b62d758c4394ce82286d01adc40c29
EunOh <bdohhhhh@gmail.com>
2025-03-15 18:37:13 +0900

    [update]: sowon 기록 post 까지 성공

--- src/components/project/star/StarForm.tsx
+++ src/components/project/star/StarForm.tsx
@@ -1,16 +1,36 @@
 'use client';
 
 import { Button } from '@/components/ui/button';
-import { FormControl, FormField, FormItem, FormMessage } from '@/components/ui/form';
+import { Form, FormControl, FormField, FormItem, FormMessage } from '@/components/ui/form';
 import { Textarea } from '@/components/ui/textarea';
+import useAuth from '@/hooks/auth/auth.hook';
+import { useStarsMutation } from '@/hooks/mutations/stars.mutation';
+import { useStarStore } from '@/stores/zustand';
+import { generateColor } from '@/utils/projects/timecapsule/generateColor';
+import { generateRandomPosition } from '@/utils/projects/timecapsule/generatePosition';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { Form, useForm } from 'react-hook-form';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
+import { useForm } from 'react-hook-form';
 import { z } from 'zod';
+import { useShallow } from 'zustand/react/shallow';
 
-const formSchema = z.object({
+const sowonSchema = z.object({
   sowon: z.string().min(2, { message: '소원은 2글자 이상 입력해주세요.' }).max(50, {
     message: '소원은 50글자 이하로 입력해주세요.',
   }),
+});
+
+const starSchema = z.object({
+  sowon: sowonSchema,
+  user_email: z.string().email(),
+  power: z.number(),
+  last_touched_at: z.string().datetime(),
+  color: z.string(),
+  positions: z.array(z.number()).length(3),
+  created_at: z.string().optional(),
+  updated_at: z.string().optional(),
+  id: z.string().optional(),
 });
 
 type StarFormProps = {
@@ -18,18 +38,59 @@ type StarFormProps = {
 };
 
 function StarForm({ button }: StarFormProps) {
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const router = useRouter();
+  const { user } = useAuth();
+  const { mutate: postStar, isPending: isPostStarPending, error, isSuccess } = useStarsMutation();
+
+  const { funnel, setFunnel } = useStarStore(
+    useShallow((state) => ({
+      funnel: state.funnel,
+      setFunnel: state.setFunnel,
+    })),
+  );
+
+  const form = useForm<z.infer<typeof sowonSchema>>({
+    resolver: zodResolver(sowonSchema),
     defaultValues: {
       sowon: '',
     },
   });
 
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values);
+  function onSubmit(values: z.infer<typeof sowonSchema>) {
+    if (!user) return;
+
+    const star = {
+      id: crypto.randomUUID(),
+      sowon: values.sowon,
+      created_at: new Date().toISOString(),
+      updated_at: new Date().toISOString(),
+      user_email: user.email,
+      power: 7,
+      color: generateColor(),
+      positions: [generateRandomPosition(), generateRandomPosition(), generateRandomPosition()],
+      last_touched_at: new Date().toISOString(),
+    };
+
+    postStar(star);
   }
+
+  useEffect(() => {
+    console.log('user 로그인되었나? ====>', user);
+    // if (!user) router.push('/star?funnel=0');
+  }, [user]);
+
+  useEffect(() => {
+    if (error) {
+      console.error('Failed to create star:', error);
+      router.push('/star?funnel=0');
+    }
+  }, [error, router]);
+
+  useEffect(() => {
+    if (isSuccess) {
+      setFunnel(funnel + 1);
+    }
+  }, [isSuccess, setFunnel, funnel]);
 
   return (
     <Form {...form}>
@@ -53,8 +114,8 @@ function StarForm({ button }: StarFormProps) {
             </FormItem>
           )}
         />
-        <Button type="submit" variant="secondary">
-          {button}
+        <Button type="submit" variant="secondary" disabled={isPostStarPending}>
+          {isPostStarPending ? '생성 중...' : button}
         </Button>
       </form>
     </Form>
---
+++ src/hooks/mutations/stars.mutation.ts
@@ -0,0 +1,24 @@
+import { postStar, putStar } from '@/apis/projects/star/star.api';
+import { QUERY_KEY_STARS } from '@/constants/query.constant';
+import { StarFromSupabase } from '@/types/projects.type';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+
+export function useStarsMutation() {
+  const queryClient = useQueryClient();
+  return useMutation<StarFromSupabase, Error, StarFromSupabase>({
+    mutationFn: (star) => postStar(star),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [QUERY_KEY_STARS] });
+    },
+  });
+}
+
+export function useEditStarsMutation() {
+  const queryClient = useQueryClient();
+  return useMutation<StarFromSupabase, Error, StarFromSupabase>({
+    mutationFn: (star) => putStar(star),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [QUERY_KEY_STARS] });
+    },
+  });
+}
