0b2fb1afe2e4c3773012e1fecb6af11d8a7996a7
Eun Oh <bdohhhhh@gmail.com>
2025-03-16 22:33:08 +0900

    [update]: stars 와 star api 구분

--- src/apis/projects/star/star.api.ts
+++ src/apis/projects/star/star.api.ts
@@ -1,10 +1,16 @@
 import { StarFromSupabase } from '@/types/projects.type';
 import fetchWrapper from '@/utils/common/fetchWrapper';
 
-export async function getStars(email: string | null) {
+export async function getStar(email: string | null) {
   if (!email) return null;
   const url = `/api/get/stars?email=${email}`;
   const data = await fetchWrapper<StarFromSupabase>(url, { method: 'GET' });
+  return data;
+}
+
+export async function getStars() {
+  const url = `/api/get/allstars`;
+  const data = await fetchWrapper<StarFromSupabase[]>(url, { method: 'GET' });
   return data;
 }
 
