ed5bacc90bb2aa1a5c36f25784873257ef322e4e
EunOh <bdohhhhh@gmail.com>
2025-03-22 15:27:07 +0900

    [update]: spheres 기본 카메라 세팅, 별 밝기 세팅

--- src/components/project/star/Spheres.tsx
+++ src/components/project/star/Spheres.tsx
@@ -4,8 +4,10 @@ import useAuth from '@/hooks/auth/auth.hook';
 import { useStarQuery, useStarsQuery } from '@/hooks/queries/star.query';
 import { useStarStore } from '@/stores/zustand';
 import { Star } from '@/types/projects.type';
+import { useFrame } from '@react-three/fiber';
 import { useEffect, useRef } from 'react';
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { useShallow } from 'zustand/react/shallow';
 import SphereStar from './Sphere';
 
@@ -26,7 +28,32 @@ function Spheres() {
 
   const groupRef = useRef<THREE.Group>(null);
   const sphereRef = useRef<THREE.Mesh>(null);
+  const cameraTargetRef = useRef<THREE.Vector3>(new THREE.Vector3());
 
+  useFrame((state, delta) => {
+    if (!groupRef.current) return;
+    // groupRef.current.rotation.y += 0.004 * delta; // 속도 조절
+
+    if (!sphereRef.current || !state.controls) return;
+    if (focusedStar?.object) {
+      const target = focusedStar.object.position.clone();
+
+      const smoothness = 0.05;
+      cameraTargetRef.current.lerp(target, smoothness);
+      state.camera.lookAt(target);
+
+      // Fov를 부드럽게 변경
+      if (state.camera instanceof THREE.PerspectiveCamera) {
+        const targetFOV = 60; // 목표 줌 레벨
+        state.camera.fov = THREE.MathUtils.lerp(state.camera.fov, targetFOV, smoothness);
+        state.camera.updateProjectionMatrix(); // Zoom 변경 후 프로젝션 매트릭스 업데이트
+      }
+      (state.controls as OrbitControls).target.copy(cameraTargetRef.current);
+      (state.controls as OrbitControls).update();
+    }
+  });
+
+  // 별의 three.js 객체를 stars 배열에 추가
   useEffect(() => {
     // 그룹을 반복문 돌면서 각 자식 요소의 이름(uuid임)을 확인하여 업데이트
     if (!groupRef.current) return;
@@ -41,8 +68,8 @@ function Spheres() {
     setStars(updatedStars as Star[]);
   }, [starsFromSupabase, setStars]);
 
-  console.log(stars);
-
+  // 포커스된 별 설정
+  // 테이블 업데이트 되면 focusedStar 도 여기서 업데이트함
   useEffect(() => {
     if (!starFromSupabase || !('id' in starFromSupabase) || !user) return;
     if (stars.length === 0) return;
@@ -50,14 +77,29 @@ function Spheres() {
     const star = stars.find((star) => star.id === starFromSupabase.id);
     if (!star) return;
 
-    setFocusedStar(star);
+    const mergedStar = { ...starFromSupabase, ...star };
+    setFocusedStar(mergedStar);
   }, [star, stars, setFocusedStar, user, starFromSupabase]);
+
+  // 포커스된 별의 밝기 조정
+  useEffect(() => {
+    groupRef.current?.traverse((child) => {
+      if (!(child instanceof THREE.Mesh)) return;
+      if (!focusedStar) {
+        (child.material as THREE.MeshStandardMaterial).emissiveIntensity = 0.01;
+        return;
+      }
+      if (child.name === focusedStar.id) {
+        (child.material as THREE.MeshStandardMaterial).emissiveIntensity = 1;
+      } else {
+        (child.material as THREE.MeshStandardMaterial).emissiveIntensity = 0.01;
+      }
+    });
+  }, [focusedStar]);
 
   return (
     <group ref={groupRef}>
-      {starsFromSupabase?.map((star) => (
-        <SphereStar ref={sphereRef} key={star.id} star={star} onClick={() => console.log(star)} />
-      ))}
+      {starsFromSupabase?.map((star) => <SphereStar ref={sphereRef} key={star.id} star={star} />)}
     </group>
   );
 }
