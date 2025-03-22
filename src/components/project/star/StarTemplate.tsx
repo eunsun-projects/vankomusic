a273729cc87cc59b225c539914de5f9d61b8a38e
EunOh <bdohhhhh@gmail.com>
2025-03-22 15:28:23 +0900

    [update]: dialog 로컬스토리지 첫접속 여부 저장

--- src/components/project/star/StarTemplate.tsx
+++ src/components/project/star/StarTemplate.tsx
@@ -1,14 +1,24 @@
 'use client';
 
 import Loading from '@/app/loading';
-import { Suspense } from 'react';
+import { Suspense, useEffect } from 'react';
 import StarCanvas from './StarCanvas';
 import StarDialog from './StarDialog';
+import StarUI from './StarUI';
 
 function StarTemplate() {
+  // 첫 접속시 로컬스토리지에 저장
+  useEffect(() => {
+    const isFirst = localStorage.getItem('isFirst');
+    if (!isFirst) {
+      localStorage.setItem('isFirst', JSON.stringify(false));
+    }
+  }, []);
+
   return (
     <Suspense fallback={<Loading />}>
       <StarDialog />
+      <StarUI />
       <section className="flex items-center justify-center h-full w-full relative z-0">
         <StarCanvas />
       </section>
