bb12bd93ce4c694818ca5336168fd2e46aa296bc
EunOh <bdohhhhh@gmail.com>
2025-12-13 21:45:05 +0900

    fix: supabase 타입에러 수정3

--- app/api/get/allstars/route.ts
+++ app/api/get/allstars/route.ts
@@ -1,25 +1,29 @@
-import { createClient } from '@/utils/supabase/server';
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from "next/server";
+import { createClient } from "@/utils/supabase/server";
 
-export async function GET(request: NextRequest) {
-  const supabase = await createClient();
-  const { data: stars, error } = await supabase.from('stars').select('*');
+export async function GET() {
+	const supabase = await createClient();
+	const { data: stars, error } = await supabase.from("stars").select("*");
 
-  if (error) {
-    return NextResponse.json({ error: error.message }, { status: 500 });
-  }
+	if (error) {
+		return NextResponse.json({ error: error.message }, { status: 500 });
+	}
 
-  const starsWithoutCredentials = stars?.map((star) => {
-    return {
-      id: star.id,
-      power: star.power,
-      created_at: star.created_at,
-      updated_at: star.updated_at,
-      color: star.color,
-      positions: star.positions,
-      last_touched_at: star.last_touched_at,
-    };
-  });
+	if (!stars) {
+		return NextResponse.json({ error: "No stars found" }, { status: 404 });
+	}
 
-  return NextResponse.json(starsWithoutCredentials, { status: 200 });
+	const starsWithoutCredentials = stars.map((star) => {
+		return {
+			id: star.id,
+			power: star.power,
+			created_at: star.created_at,
+			updated_at: star.updated_at,
+			color: star.color,
+			positions: star.positions,
+			last_touched_at: star.last_touched_at,
+		};
+	});
+
+	return NextResponse.json(starsWithoutCredentials, { status: 200 });
 }
